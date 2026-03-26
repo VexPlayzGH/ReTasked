4cf7cc9524f26394fc6562d0e3f567b36fe45441
VexPlayzGH <johnionmr@gmail.com>
2026-03-26 21:31:56 +0200

    Update to ver. 0.2.0
    tweaked some of the logic, added a "List Saved!" paragraph widget on save.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,21 @@
 use color_eyre::eyre::{Ok, Result};
 use ratatui::{
-    crossterm::event::{self, Event, KeyEvent},
-    layout::{Constraint, Layout},
-    prelude::Direction,
-    style::{Color, Modifier, Style, Stylize},
-    text::ToSpan,
-    widgets::{Block, BorderType, Borders, List, ListItem, ListState, Padding, Paragraph, Widget},
-    DefaultTerminal,
-    Frame};
+    DefaultTerminal, Frame, crossterm::event::{self, Event, KeyEvent}, layout::{Alignment, Constraint, Layout}, prelude::Direction, style::{Color, Modifier, Style, Stylize}, text::ToSpan, widgets::{Block, BorderType, Borders, List, ListItem, ListState, Padding, Paragraph, Widget}};
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
+use std::time::{Duration, Instant};
 
 #[derive(Debug, Default)]
-struct AppState {
+struct AppState { // Store each App state into a struct
     items: Vec<TodoItem>,
     list_state: ListState,
     is_add_new: bool,
     is_help: bool,
     input_value: String,
+    show_save_message: bool,
+    save_message_time: Option<Instant>,
 }
 
 #[derive(Debug, Default)]
@@ -46,46 +42,48 @@ fn main() -> Result<()> {
     result
 }
 
-fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
+fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> { // runs every frame and calls event handlers
     loop {
         //Rendering
         terminal.draw(|f| render(f, app_state))?;
         //Input Handling
-        if let Event::Key(key) = event::read()? {
-            if app_state.is_add_new {
-                match handle_add_new(key, app_state) {
-                    FormAction::None => {},
-                    FormAction::Submit => {
-                        app_state.is_add_new = false;
-                        app_state.items.push(TodoItem {
-                            is_done: false,
-                            description: format!("• {}", app_state.input_value.clone())
-                        });
-                        app_state.input_value.clear();
-                    },
+        if event::poll(Duration::from_millis(100))? {
+            if let Event::Key(key) = event::read()? {
+                if app_state.is_add_new {
+                    match handle_add_new(key, app_state) {
+                        FormAction::None => {},
+                        FormAction::Submit => {
+                            app_state.is_add_new = false;
+                            app_state.items.push(TodoItem {
+                                is_done: false,
+                                description: format!("• {}", app_state.input_value.clone())
+                            });
+                            app_state.input_value.clear();
+                        },
+                    }
                 }
-            }
-            else if app_state.is_help {
-                handle_help(key, app_state);
-            }
-            else {
-                if handle_key(key, app_state) {
-                    break;
+                else if app_state.is_help {
+                    handle_help(key, app_state);
                 }
-            }            
+                else {
+                    if handle_key(key, app_state) {
+                        break;
+                    }
+                }            
+            }
         }
     }
 
     Ok(())
 }
 
-fn write_file(file_path: &str, contents: &str) -> Result<()> {
+fn write_file(file_path: &str, contents: &str) -> Result<()> { // writes to a file (used for the list save)
     let mut file = File::create(file_path)?;
     file.write_all(contents.as_bytes())?;
     Ok(())
 }
 
-fn handle_help(key: KeyEvent, app_state: &mut AppState) {
+fn handle_help(key: KeyEvent, app_state: &mut AppState) { // handles the help menu toggle
     match key.code {
         event::KeyCode::Esc => {
             if key.kind == event::KeyEventKind::Press {
@@ -96,7 +94,7 @@ fn handle_help(key: KeyEvent, app_state: &mut AppState) {
     }    
 }
 
-fn handle_add_new(key: KeyEvent, app_state: &mut AppState) -> FormAction {
+fn handle_add_new(key: KeyEvent, app_state: &mut AppState) -> FormAction { // handles K/B input on Input Mode
     match key.code {
         event::KeyCode::Char(c) => {
             if key.kind == event::KeyEventKind::Press {
@@ -124,9 +122,8 @@ fn handle_add_new(key: KeyEvent, app_state: &mut AppState) -> FormAction {
     }
     FormAction::None
 }
-
-fn handle_key(key: KeyEvent, app_state: &mut AppState) -> bool {
-    match key.code {
+fn handle_key(key: KeyEvent, app_state: &mut AppState) -> bool { // Handles K/B input on View Mode
+    match key.code { 
         event::KeyCode::F(1) | event::KeyCode::Char('H') | event::KeyCode::Char('h') => {
             if key.kind == event::KeyEventKind::Press {
                 app_state.is_help = true;
@@ -164,7 +161,9 @@ fn handle_key(key: KeyEvent, app_state: &mut AppState) -> bool {
                     match write_file("output.txt", &contents) {
                         _ => {},
                     }
-                }   
+                    app_state.show_save_message = true;
+                    app_state.save_message_time = Some(Instant::now());
+                }  
             },
             'a' | 'A' => {
                 if key.kind == event::KeyEventKind::Press {
@@ -195,7 +194,7 @@ fn handle_key(key: KeyEvent, app_state: &mut AppState) -> bool {
     false
 }
 
-fn get_prev_list(app_state: &mut AppState) -> Result<()> {
+fn get_prev_list(app_state: &mut AppState) -> Result<()> { // Manages the display of the already stored list in "output.txt"
     let file_path = Path::new("output.txt");
     let file = File::open(file_path)?;
     let reader = BufReader::new(file);
@@ -209,7 +208,7 @@ fn get_prev_list(app_state: &mut AppState) -> Result<()> {
     Ok(())
 }
 
-fn render(frame: &mut Frame, app_state: &mut AppState) {    
+fn render(frame: &mut Frame, app_state: &mut AppState) { // renders all widgets  
     let layout = Layout::default()
         .direction(Direction::Horizontal)
         .constraints(vec![
@@ -283,11 +282,29 @@ fn render(frame: &mut Frame, app_state: &mut AppState) {
             .title("VIEW MODE // View the list // Press F1/H to show keybinds".to_span().into_centered_line())
             .fg(Color::Rgb(255, 255, 0))
             .render(border_area, frame.buffer_mut());
+        
+        let list_area = if app_state.show_save_message {
+            if let Some(show_time) = app_state.save_message_time {
+                if show_time.elapsed().as_secs() >= 3 {
+                    app_state.show_save_message = false;
+                    app_state.save_message_time = None;
+                }
+            }
+            Layout::vertical([
+                Constraint::Fill(1),
+                Constraint::Length(1)
+                ])
+                .split(inner_area)[0]
+        } else {
+            inner_area
+        };
+
         let list = List::new(app_state.items
             .iter()
             .map(|x| {
                 let value = if x.is_done {
-                   x.description.to_span().add_modifier(Modifier::BOLD).style(Style::default().fg(Color::Rgb(255, 0, 0))).crossed_out()
+                   x.description.to_span().add_modifier(Modifier::BOLD)
+                   .style(Style::default().fg(Color::Rgb(255, 0, 0))).crossed_out()
                 } else {
                    x.description.to_span() 
                 };
@@ -296,6 +313,21 @@ fn render(frame: &mut Frame, app_state: &mut AppState) {
                 .highlight_symbol(">")
                 .highlight_style(Style::default().add_modifier(Modifier::BOLD | Modifier::ITALIC).fg(Color::Green));
 
-        frame.render_stateful_widget(list, inner_area, &mut app_state.list_state);
+        frame.render_stateful_widget(list, list_area, &mut app_state.list_state);
+
+        if app_state.show_save_message {
+            if let Some(show_time) = app_state.save_message_time {
+                if show_time.elapsed().as_secs() < 3 { // keeps the notification on save displayed for 3 seconds
+                    let message_area = Layout::vertical([Constraint::Fill(1), Constraint::Length(1)])
+                        .split(inner_area)[1];
+                    frame.render_widget(
+                        Paragraph::new("List Saved!")
+                        .alignment(Alignment::Center)
+                        .style(Style::default().fg(Color::Rgb(255, 255, 0))),
+                        message_area
+                    );
+                }
+            }
+        }
     }
 }
